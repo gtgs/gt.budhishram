491ac94b95437b7aafea82492cda56c9905daf9a
Girish Sarwal <g.sarwal@gmail.com>
2015-06-13 14:31:04 +0530

    temporarily added old code

---
+++ index.php
@@ -0,0 +1,104 @@
+<?php
+?>
+<html>
+<head>
+<link rel="stylesheet" href="styles/stylesheet.css">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<link  href="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.2/fotorama.css" rel="stylesheet"> <!-- 3 KB -->
+<script src="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.2/fotorama.js"></script> <!-- 16 KB -->
+</head>
+<body>
+	<div id="top">
+		<div id="logo">
+			<img src="images/logo.png" height="80px" width="200px"></img>
+			<ul style="list-style:none;float:right;">
+				<li style="display:inline;margin-left:10px;"><a href="#explore">explore</a></li>
+				<li style="display:inline;margin-left:10px;"><a href="#network">our network</a></li>
+				<li style="display:inline;margin-left:10px;"><a href="#subscribe">subscribe</a></li>
+				<li style="display:inline;margin-left:10px;"><a href="#team">team</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="loginbar"> 
+		<center>
+		<div style="float:center;">
+		<i>Login to explore the fun!.</i>
+		<input type="text" value="Email"></input>
+		<input type="text" value="Password"></input>
+		<span class="smallbutton">
+			Login
+		</span>
+		<span class="smallbutton-invert">
+			Sign up
+		</span>
+		</div>
+		</center>
+	</div>
+	<div class="bigheading">
+	<center>
+			Is your child a screenager?<br/>
+			Make their screentime useful and progressive
+		</center>
+	</div>
+	<div id="content" id="home">
+	<center>
+		<div class="fotorama" data-width="1000px" data-height="500px" data-autoplay="2000">
+			<div data-img="images/kid-tablet-1.jpg">
+				<span class="largebutton" style="margin-top:400px;margin-left:400px;">
+					Jump on
+				</span>	
+			</div>
+			<div data-img="images/kid-tablet-2.jpg">
+				<span class="largebutton" style="margin-top:400px;margin-left:400px;">
+					Jump on
+				</span>	
+			</div>
+			<div data-img="images/kid-tablet-3.jpg">
+				<span class="largebutton" style="margin-top:400px;margin-left:400px;">
+					Jump on
+				</span>	
+			</div>
+			<div data-img="images/kid-tablet-4.jpg">
+				<span class="largebutton" style="margin-top:400px;margin-left:400px;">
+					Jump on
+				</span>	
+			</div>
+			<div data-img="images/kid-tablet-5.jpg">
+				<span class="largebutton" style="margin-top:400px;margin-left:400px;">
+					Jump on
+				</span>	
+			</div>
+		</div>
+	</div>
+	<div id="explore" style="height:600px;background:#1d34c0;">
+	
+	</div>
+	<div id="network" style="height:600px;background:#f987e5;">
+	
+	</div>
+	<div id="subscribe" style="height:600px;background:#fff600;">
+	
+	</div>
+	<div id="team" style="height:600px;background:#f67b4a;">
+	
+	</div>
+	
+	
+	
+	<div id="footer">
+	<div id="social">
+		<img src="images/twitter.png">
+		<img src="images/facebook.png">
+		<img src="images/google+.png">
+		<img src="images/youtube.png">
+		<img src="images/linkedin.png">
+		<img src="images/rss.png">
+	</div>
+	<center>
+		copyright &copy; budhishram 2014
+		<a href="#top">about us</a> | <a href="#top">t&c</a> | <a href="#top">privacy</a> | <a href="#top">contact us</a>
+	</center>
+	
+	</div>
+</body>
+</html>
